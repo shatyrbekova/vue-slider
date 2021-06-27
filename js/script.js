9839b0884bf523089be1909ab9c8a9f48333b3da
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-27 11:06:58 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -11,8 +11,17 @@ new Vue({
            'img/image3.jpeg',
            'img/image4.jpeg',
            'img/image5.jpeg',
+           'img/image6.jpeg'
         ],
         photoIndex: 0,
+       photosSorrento:[
+        'img/image7.jpeg',
+        'img/image8.jpeg',
+        'img/image9.jpeg',
+        'img/image10.jpeg',
+        'img/image11.jpeg',
+        'img/image12.jpeg',
+       ]
     },
 
     created(){
